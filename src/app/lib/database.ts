a52642a5d9ef8d6f955f1a8e00f08981b3836c5d
Max Cheung <maxcheung07@gmail.com>
2025-02-06 18:31:46 -0800

    cleaned up some typescript linting errors, not quite necessary but bugged me

--- src/app/lib/database.ts
+++ src/app/lib/database.ts
@@ -1,24 +1,20 @@
 'use server'
 
-import mysql from 'mysql2/promise';
+import mysql, { ConnectionOptions } from 'mysql2/promise';
 import { NextResponse } from 'next/server';
 
 
-const dbConfig = {
+const dbConfig: ConnectionOptions = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
 }
 
-let dbConnection: {};
-
 export async function connectDB() {
-  if(!dbConnection) {
-    dbConnection = await mysql.createConnection(dbConfig);
-    console.log(`Connected to MYSQL Database: ${process.env.DB_NAME}`);
-  }
+  const dbConnection = await mysql.createConnection(dbConfig);
+  console.log(`Connected to MYSQL Database: ${process.env.DB_NAME}`);
   return dbConnection;
 }
 
