785dc4acc5b96b48e00eb4e027245e30b3dbd7c6
Max Cheung <maxcheung07@gmail.com>
2025-02-10 18:42:34 -0800

    added 'zod' for server side input validation, still experimenting with returning the right argument to the client to not get overloading error

--- src/app/lib/database.ts
+++ src/app/lib/database.ts
@@ -1,6 +1,24 @@
 'use server';
 
 import mysql, { ConnectionOptions } from 'mysql2/promise';
+import { z } from 'zod';
+
+const FormSchema = z.object({
+  license_plate: z.string().regex(/^[a-zA-Z0-9]{1,7}$/, {
+    message: "Please enter a valid license plate #."
+  })
+});
+
+const ReportLicense = FormSchema;
+
+export type State = {
+  errors?: {
+    license_plate?: string[];
+  };
+  message?: string | null;
+}
+
+
 
 const dbConfig: ConnectionOptions = {
   host: process.env.DB_HOST,
@@ -34,20 +52,34 @@ export async function GET() {
   }
 }
 
-export async function createPost(formData: FormData) {
+export async function reportLicense(prevState: State, formData: FormData) {
   // connect to db
   const db = await connectDB();
 
   // brakes boolean set to 1 as default for reporting "brakes" as problem
   const brakes = 1;
-  const license_plate = formData.get('license');
+
+  const validatedFields = ReportLicense.safeParse({
+    license_plate: formData.get('license')
+  })
+
+  if(!validatedFields.success) {
+    console.log("error")
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Invalid Fields. Failed to Report License"
+    }
+  }
+   
+  const { license_plate } = validatedFields.data;
 
   const sql = 'INSERT INTO vehicles (license_plate, brakes) VALUES (?, ?)';
 
   console.log(license_plate);
   try {
-    const result = await db.execute(sql, [license_plate, brakes]);
-    console.log(result);
+    const rows = await db.execute(sql, [license_plate, brakes]);
+    console.log(rows);
+    return JSON.parse(JSON.stringify(rows)); 
   } catch (error) {
     console.error(error);
   }
--- src/app/ui/report-form.tsx
+++ src/app/ui/report-form.tsx
@@ -1,8 +1,18 @@
-import { createPost } from '@/app/lib/database'
+'use client'
+
+import { reportLicense, State } from '@/app/lib/database'
+import { useActionState } from 'react';
 
 export default function Form() {
+  const initialState: State = { message: null, errors: {} };
+
+  const [state, formAction] = useActionState(reportLicense, initialState);
+
+  console.log(state);
+  console.log("hello world");
+
   return (
-    <form action={createPost} className="font-[family-name:var(--font-geist-mono)]">
+    <form action={formAction} className="font-[family-name:var(--font-geist-mono)]">
       <label>Report</label>
       <input type="text" id="license" name="license" placeholder="License #" className="text-black" />
       <button type="submit">Submit</button>
