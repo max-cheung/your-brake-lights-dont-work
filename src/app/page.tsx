9773729a1acb1a2cbc56e511df2ce7f768ac6fb3
Max Cheung <maxcheung07@gmail.com>
2025-02-14 14:06:59 -0800

    commented out dark pref in global.css, changed some css, added report-form errors and messages to client

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Form from "@/app/ui/report-form";
 // font-[family-name:var(--font-geist-mono)]
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center w-full h-screen border-2 border-solid">
-      <h1 className="font-[family-name:var(--font-geist-mono)]">
+    <main className="flex flex-col items-center justify-center w-full h-screen">
+      <h1 className="font-[family-name:var(--font-geist-mono)] mb-2">
         Your Brake Lights Don't Work.
       </h1>
       <Form />
--- src/app/ui/report-form.tsx
+++ src/app/ui/report-form.tsx
@@ -1,22 +1,43 @@
-'use client'
+"use client";
 
-import { reportLicense, State } from '@/app/lib/action'
-import { useActionState, useEffect } from 'react';
+import { reportLicense, State } from "@/app/lib/action";
+import { useActionState, useEffect } from "react";
 
 export default function Form() {
   const initialState: State = { errors: {}, message: null };
 
   const [state, formAction] = useActionState(reportLicense, initialState);
+  const str: string = 'hello world'
 
   useEffect(() => {
     console.log(JSON.stringify(state));
   }, [state]);
 
   return (
-    <form action={formAction} className="font-[family-name:var(--font-geist-mono)]">
-      <label>Report</label>
-      <input type="text" id="license" name="license" placeholder="License #" className="text-black" pattern='^(?!\s)(?!.*\s$)[a-zA-Z0-9 ]{1,7}$' required title="Input a valid license plate."/>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form
+        action={formAction}
+        className="font-[family-name:var(--font-geist-mono)]"
+      >
+        <label>Report</label>
+        <input
+          type="text"
+          id="license"
+          name="license"
+          placeholder="License #"
+          className="border-2"
+          pattern="^(?!\s)(?!.*\s$)[a-zA-Z0-9 ]{1,7}$"
+          required
+          title="Input a valid license plate."
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        {!state.errors && <p>{state.message}</p>}
+      </div>
+      <div className="font-[family-name:var(--font-geist-mono) mt-2 text-red-600">
+        {state.errors?.license_plate && <p>{state.errors.license_plate}</p>}
+      </div>
+    </>
   );
 }
