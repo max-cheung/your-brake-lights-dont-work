2215f42d3240955fe4352dbf9df41473736056f8
Max Cheung <maxcheung07@gmail.com>
2025-02-13 17:40:51 -0800

    cleaned up action.ts

--- src/app/lib/action.ts
+++ src/app/lib/action.ts
@@ -38,38 +38,32 @@ export async function reportLicense(prevState: State, formData: FormData) {
   // connect to db
   const db = await connectDB();
 
-  // brakes boolean set to 1 as default for reporting "brakes" as problem
-  const brakes = 1;
-
   const validatedFields = ReportLicense.safeParse({
     license_plate: formData.get('license')
-  })
+  });
 
   if(!validatedFields.success) {
-    console.log("error")
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: "Invalid Fields. Failed to Report License"
-    }
+    };
   }
    
+  // brakes boolean set to 1 as default for reporting "brakes" as problem
+  const brakes = 1;
   const { license_plate } = validatedFields.data;
 
   const sql = 'INSERT INTO vehicles (license_plate, brakes) VALUES (?, ?)';
 
-  console.log(license_plate);
   try {
     const rows = await db.execute(sql, [license_plate, brakes]);
     console.log(rows);
-    // return JSON.parse(JSON.stringify(rows)); 
     return {
-      errors: {},
       message: "success"
     }
   } catch (error) {
     console.error(error);
     return {
-      // errors: {},
       message: 'failed'
     }
   }
