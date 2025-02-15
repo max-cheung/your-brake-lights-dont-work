93c2d7003b89012f3f70c60b4f4b79f52e504005
Max Cheung <maxcheung07@gmail.com>
2025-02-14 21:12:06 -0800

    refined css for report-form, added initial search form html elements to search-form

--- src/app/ui/report-form.tsx
+++ src/app/ui/report-form.tsx
@@ -19,18 +19,18 @@ export default function Form() {
         action={formAction}
         className="font-[family-name:var(--font-geist-mono)]"
       >
-        <label>Report</label>
+        <label hidden>Report</label>
         <input
           type="text"
           id="license"
           name="license"
-          placeholder="License #"
-          className="border-2"
+          placeholder="Report License #"
+          className="border-2 pl-2 rounded-md"
           // pattern="^(?!\s)(?!.*\s$)[a-zA-Z0-9 ]{1,7}$"
           required
           title="Input a valid license plate."
         />
-        <button type="submit">Submit</button>
+        <button type="submit" className="border-2 rounded-md px-2 ml-2">Submit</button>
       </form>
       <div>
         {!state.errors && state.message==="Database: Post Request Successful!" && <p className="mt-2 font-[family-name:var(--font-geist-mono)]">Thank you for helping make the roads safer!</p>}
--- src/app/ui/search-form.tsx
+++ src/app/ui/search-form.tsx
@@ -1,5 +1,15 @@
 export default function Form() {
   return (
-    <h1>Search</h1>
-  )
+    <form>
+      <label hidden>Check Your License</label>
+      <input
+        type="text"
+        id="checkLicense"
+        name="checkLicense"
+        className="border-2 rounded-md pl-2 font-[family-name:var(--font-geist-mono)]"
+        placeholder="Check License #"
+      />
+      <button className="font-[family-name:var(--font-geist-mono)] border-2 rounded-md px-2 ml-2">Submit</button>
+    </form>
+  );
 }
